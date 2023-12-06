340bc7c5e6e6081e062ba9da5e4bf809d6e41789
haole2k4 <ahihihaodepzai@gmail.com>
2023-12-06 20:25:04 +0700

    Update Search img
    thay đổi hình phần tìm kiếm và thêm hàm đổ màu cho thẻ của mục tìm kiếm

--- scripts/script.js
+++ scripts/script.js
@@ -23,78 +23,85 @@ greeting.innerHTML = welcomeText;
 
 
 //Scrolling nav bar code
-const nav=document.querySelector("#topNav");
-const sectionOne=document.querySelector(".fw-bold");
-const sectionOneOptions={};
-const sectionOneObserver = new IntersectionObserver(function(entries , sectionOneObserver) {
+const nav = document.querySelector("#topNav");
+const sectionOne = document.querySelector(".fw-bold");
+const sectionOneOptions = {};
+const sectionOneObserver = new IntersectionObserver(function (entries, sectionOneObserver) {
   entries.forEach(entry => {
-    if(!entry.isIntersecting){      
-      nav.style.backgroundColor="black";
-    } else{     
-      nav.style.backgroundColor="transparent";
+    if (!entry.isIntersecting) {
+      nav.style.backgroundColor = "black";
+    } else {
+      nav.style.backgroundColor = "transparent";
     }
   })
-} , sectionOneOptions);
+}, sectionOneOptions);
 sectionOneObserver.observe(sectionOne);
 
 
-  function playAudio() {
-    var audioPath = "./mp3/1.mp3"; // Đường dẫn của file MP3
-    var audioPlayer = document.getElementById("audioPlayer");
-    var audioSource = document.getElementById("audioSource");
 
-    audioSource.src = audioPath;
-    audioPlayer.load(); // Load lại nguồn âm thanh
-    audioPlayer.play();
+
+
+
+
+
+
+function playAudio() {
+  var audioPath = "./mp3/1.mp3"; // Đường dẫn của file MP3
+  var audioPlayer = document.getElementById("audioPlayer");
+  var audioSource = document.getElementById("audioSource");
+
+  audioSource.src = audioPath;
+  audioPlayer.load(); // Load lại nguồn âm thanh
+  audioPlayer.play();
+}
+
+var audioPlayer = document.getElementById("audioPlayer"); // Thay thế "audioPlayer" bằng ID của thẻ audio của bạn
+var progressSlider = document.getElementById("progressSlider");
+var currentTimeElement = document.getElementById("currentTime");
+var durationElement = document.getElementById("duration");
+
+var isSeeking = false; // Biến để kiểm tra xem người dùng có đang tua hay không
+
+function updateProgressBar() {
+  var progress = progressSlider.value / 100;
+
+  if (!isSeeking) {
+    audioPlayer.currentTime = progress * audioPlayer.duration;
   }
 
-  var audioPlayer = document.getElementById("audioPlayer"); // Thay thế "audioPlayer" bằng ID của thẻ audio của bạn
-  var progressSlider = document.getElementById("progressSlider");
-  var currentTimeElement = document.getElementById("currentTime");
-  var durationElement = document.getElementById("duration");
+  updateTimeDisplay();
+}
 
-  var isSeeking = false; // Biến để kiểm tra xem người dùng có đang tua hay không
+function updateTimeDisplay() {
+  var currentTime = formatTime(audioPlayer.currentTime);
+  var duration = formatTime(audioPlayer.duration);
 
-  function updateProgressBar() {
-    var progress = progressSlider.value / 100;
-    
-    if (!isSeeking) {
-      audioPlayer.currentTime = progress * audioPlayer.duration;
-    }
+  currentTimeElement.textContent = currentTime;
+  durationElement.textContent = duration;
+}
 
+function formatTime(time) {
+  var minutes = Math.floor(time / 60);
+  var seconds = Math.floor(time % 60);
+  return (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+}
+
+// Sự kiện để cập nhật thanh trượt và thời gian hiển thị khi thời lượng bài hát thay đổi
+audioPlayer.addEventListener("timeupdate", function () {
+  if (!isSeeking) {
+    var progress = (audioPlayer.currentTime / audioPlayer.duration) * 100;
+    progressSlider.value = progress;
     updateTimeDisplay();
   }
+});
 
-  function updateTimeDisplay() {
-    var currentTime = formatTime(audioPlayer.currentTime);
-    var duration = formatTime(audioPlayer.duration);
+// Sự kiện bắt đầu tua
+progressSlider.addEventListener("input", function () {
+  isSeeking = true;
+  updateProgressBar();
+});
 
-    currentTimeElement.textContent = currentTime;
-    durationElement.textContent = duration;
-  }
-
-  function formatTime(time) {
-    var minutes = Math.floor(time / 60);
-    var seconds = Math.floor(time % 60);
-    return (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-  }
-
-  // Sự kiện để cập nhật thanh trượt và thời gian hiển thị khi thời lượng bài hát thay đổi
-  audioPlayer.addEventListener("timeupdate", function () {
-    if (!isSeeking) {
-      var progress = (audioPlayer.currentTime / audioPlayer.duration) * 100;
-      progressSlider.value = progress;
-      updateTimeDisplay();
-    }
-  });
-
-  // Sự kiện bắt đầu tua
-  progressSlider.addEventListener("input", function () {
-    isSeeking = true;
-    updateProgressBar();
-  });
-
-  // Sự kiện kết thúc tua
-  progressSlider.addEventListener("change", function () {
-    isSeeking = false;
-  });
+// Sự kiện kết thúc tua
+progressSlider.addEventListener("change", function () {
+  isSeeking = false;
+});
