d67018da909f2306f69955fdb5b1cf11ddc1d6b8
Quyt <92991639+ChrisNguyen25@users.noreply.github.com>
2023-12-14 22:46:42 +0700

    update đăng ký, nhập user và pass đúng vẫn báo lỗi

--- scripts/login.js
+++ scripts/login.js
@@ -1,28 +1,24 @@
 function login() {
-    // Retrieve username and password from local storage
-    var storedUsername = localStorage.getItem('email');
-    var storedPassword = localStorage.getItem('password');
+    var enteredEmail = document.getElementById('login-email').value;
+    var enteredPassword = document.getElementById('login-password').value;
 
-    // Assume you have a function to validate the login
-    if (validateLogin(storedUsername, storedPassword)) {
-        // Redirect to index or perform other actions
+    if (validateLogin(enteredEmail, enteredPassword)) {
         redirectToIndex();
     } else {
-        alert('Invalid username or password');
+        alert('Invalid email or password');
     }
 }
+console.log("hello world");
+function validateLogin(email, password) {
 
-function validateLogin(username, password) {
-    // Implement your own validation logic here
-    // For example, compare with a predefined username and password
-    var predefinedUsername = 'exampleUser';
+    var predefinedEmail = 'exampleUser';
     var predefinedPassword = 'examplePassword';
 
-    return username === predefinedUsername && password === predefinedPassword;
+    return email === predefinedEmail && password === predefinedPassword;
 }
 
 function redirectToIndex() {
-    // Implement your redirection logic here
-    // For example, window.location.href = 'index.html';
+    window.location.href = 'index.html';
     console.log('Redirecting to index...');
+    
 }
